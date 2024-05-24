19b87d0766a4813cc0e04b6dcc0d7f8ac74d455e
Boris Kundu <60821687+boriskundu@users.noreply.github.com>
2024-05-24 03:40:47 -0400

    Added search tool for agent

---
+++ LLM/LangChain/SearchAgents/tools/tools.py
@@ -0,0 +1,6 @@
+from langchain_community.tools.tavily_search import TavilySearchResults
+
+def get_profile_url_tavily(name):
+    search = TavilySearchResults()
+    res = search.run(f'{name}')
+    return res[0]["url"]
